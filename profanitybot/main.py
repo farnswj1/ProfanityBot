f2756883f1b33fa12176b0ea6e25a673386da8c7
Justin Farnsworth <farnswj1@tcnj.edu>
2022-11-19 12:07:08 -0500

    updated discord.py

--- profanitybot/main.py
+++ profanitybot/main.py
@@ -1,7 +1,8 @@
-from discord import Client
+from discord import Client, Intents
 from profanity_filter import ProfanityFilter
 import logging
 import os
+
 
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
@@ -14,11 +15,14 @@ class ProfanityBot(Client):
         logger.info(f'{self.user} is online!')
 
     async def on_message(self, message):
-        if self.user != message.author and self.profanity_filter.is_profane(message.content):
-            logger.info(f'{message.author}: {message.content}')
-            await message.channel.send(f'Watch your mouth, {message.author.mention}!')
+        author = message.author
+        content = message.content
+
+        if self.user != author and self.profanity_filter.is_profane(content):
+            logger.info(f'{author}: {content}')
+            await message.channel.send(f'Watch your mouth, {author.mention}!')
 
 
 if __name__ == '__main__':
-    client = ProfanityBot()
+    client = ProfanityBot(intents=Intents.default())
     client.run(os.environ.get('DISCORD_TOKEN'))
