bfa42b4270940a17e65bd561f781a11b6fd6856c
Justin Farnsworth <fjj12697@gmail.com>
2024-11-09 12:45:16 -0500

    rewrite bot in Rust

--- README.md
+++ README.md
@@ -1,22 +1,16 @@
 # Profanity Bot
-This is a Discord bot that identifies messages sent to it which contain profanity.
-
+This is a Discord bot that identifies messages containing profanity.
 
 ## Requirements
-This project uses Docker and Docker Compose. Please install them before continuing.
-Also, you must have a Discord bot set up in order to use this program.
-
+This project uses Docker and Docker Compose. Please install them before continuing. Also, you must have a Discord bot set up in order to use this program.
 
 ## Setup
-First, create a ```.env``` file inside the ```profanitybot``` directory.
-Then, add the token from your Discord bot to ```DISCORD_TOKEN```, as shown below:
+First, create a ```.env``` file inside the ```app``` directory. Then, add the token from your Discord bot to ```DISCORD_TOKEN```, as shown below:
 ```
 DISCORD_TOKEN=[token goes here]
 ```
 
 Then, run ```docker-compose up -d --build``` to build the image and run the container.
 
-
 ## How to Use
-Once the container is running, send a message to the Discord bot. It will only respond
-to you if your message contains any profanity.
+Once the container is running, send a message to the Discord bot. It will only respond to you if your message contains any profanity.
---
+++ app/src/main.rs
@@ -0,0 +1,59 @@
+use std::env;
+
+use dotenvy::dotenv;
+use rustrict::CensorStr;
+use serenity::all::Mentionable;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::{Client, Context, EventHandler, GatewayIntents};
+use tracing::{error, info};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, context: Context, msg: Message) {
+        // Ignore all messages created by the bot itself.
+        if msg.author.id == context.cache.current_user().id {
+            return;
+        }
+
+        // Do nothing if the message doesn't contain profanity.
+        if !msg.content.is_inappropriate() {
+            return;
+        }
+
+        info!("{}: {}", msg.author.name, msg.content);
+        let response = format!("Watch your mouth, {}!", msg.author.mention());
+        let dm = msg.channel_id.say(&context.http, response).await;
+
+        if let Err(error) = dm {
+            error!("Error when direct messaging user: {error}");
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} #{} is online!", ready.user.name, ready.user.discriminator.unwrap());
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    tracing_subscriber::fmt::init();
+
+    let token = env::var("DISCORD_TOKEN").expect("Discord token");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(error) = client.start().await {
+        error!("Client error: {error}");
+    }
+}
