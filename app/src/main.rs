8ff90d15ab10306128cbffb5f41f334ef2d8e8e0
Justin Farnsworth <fjj12697@gmail.com>
2024-11-11 11:17:07 -0500

    use tag() to display bot username

--- app/src/main.rs
+++ app/src/main.rs
@@ -34,7 +34,7 @@ impl EventHandler for Handler {
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
-        info!("{} #{} is online!", ready.user.name, ready.user.discriminator.unwrap());
+        info!("{} is online!", ready.user.tag());
     }
 }
 
